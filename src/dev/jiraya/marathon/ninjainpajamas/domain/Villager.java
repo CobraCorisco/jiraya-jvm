8acb2e63a39e43214d514f5809ead8aa55dd3e1d
CobraCorisco <petry7359@gmail.com>
2025-08-29 10:16:20 -0300

    Completed lesson on method overloading.

--- src/dev/jiraya/marathon/ninjainpajamas/domain/Villager.java
+++ src/dev/jiraya/marathon/ninjainpajamas/domain/Villager.java
@@ -3,6 +3,17 @@ package dev.jiraya.marathon.ninjainpajamas.domain;
 public class Villager {
     private String name;
     private int age;
+    private RANK rank;
+
+    public void init(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void init(String name, int age, RANK rank) {
+        this.init(name, age);
+        this.rank = rank;
+    }
 
     public String getName() {
         return name;
@@ -24,7 +35,19 @@ public class Villager {
         this.age = age;
     }
 
+    public RANK getRank() {
+        return rank;
+    }
+
+    public void setRank(RANK rank) {
+        this.rank = rank;
+    }
+
     public void show() {
+        if (this.rank != null) {
+            System.out.printf("Name: %s | age: %d | rank: %s\n", this.name, this.age, this.rank);
+            return;
+        }
         System.out.printf("Name: %s | age: %d\n", this.name, this.age);
     }
 }
--- src/dev/jiraya/marathon/ninjainpajamas/playground/VillagerPlayground.java
+++ src/dev/jiraya/marathon/ninjainpajamas/playground/VillagerPlayground.java
@@ -1,12 +1,18 @@
 package dev.jiraya.marathon.ninjainpajamas.playground;
 
+import dev.jiraya.marathon.ninjainpajamas.domain.RANK;
 import dev.jiraya.marathon.ninjainpajamas.domain.Villager;
 
 public class VillagerPlayground {
     public static void main(String[] args) {
         Villager boruto = new Villager();
         boruto.setName("Boruto");
-        boruto.setAge(10);
+        boruto.setAge(12);
         boruto.show();
+
+        Villager himawari = new Villager();
+        himawari.init("Himawari", 9);
+        himawari.init("Himawari", 9, RANK.Genin);
+        himawari.show();
     }
 }
